bd271a2202747fb47849753af9757d2089fa9a7c
qiqihaobenben <chenfangxu_qixin@163.com>
2016-07-13 18:03:33 +0800

    v 2.8.2

--- dataexchange/js/index.js
+++ dataexchange/js/index.js
@@ -21,7 +21,7 @@ $(function (){
     $('#text').on('keyup',function (){
         if($(this).val()){
             var $sc = $('<script></script>');
-            var str = 'sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su?wd='+$(this).val()+'&json=1&p=3&sid=1434_18203_20515_18240_20536_17944_20538_20415_20455_19861_17001_15168_11815&req=2&bs=%E8%A5%BF%E4%BA%8C%E6%97%97%E5%A4%96%E5%8C%85%E5%85%AC%E5%8F%B8&pbs=%E8%A5%BF%E4%BA%8C%E6%97%97%E5%A4%96%E5%8C%85%E5%85%AC%E5%8F%B8&csor=1&cb=baidu';
+            var str = 'https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su?wd='+$(this).val()+'&json=1&p=3&sid=1434_18203_20515_18240_20536_17944_20538_20415_20455_19861_17001_15168_11815&req=2&bs=%E8%A5%BF%E4%BA%8C%E6%97%97%E5%A4%96%E5%8C%85%E5%85%AC%E5%8F%B8&pbs=%E8%A5%BF%E4%BA%8C%E6%97%97%E5%A4%96%E5%8C%85%E5%85%AC%E5%8F%B8&csor=1&cb=baidu';
             $sc.attr({'src':str,'id':'sc'});
             $('body').append($sc);
         }else {
@@ -29,62 +29,62 @@ $(function (){
         }
     });
 });
-window.onload = function (){
-    var off = false;
-    var curTime = new Date().getTime()*1000;
-    ajax();
-
-    window.onscroll = function (){
-        if(off){
-            //off = false;
-            console.log($('#newsList').height() ,window.pageYOffset)
-            if($('#newsList').height() <= window.innerHeight + window.pageYOffset){
-                ajax();
-            }
-        }
-    };
-    function ajax(){
-        $.ajax({
-            url:'https://api.1-blog.com/biz/bizserver/news/list.do',
-            dataType:'json',
-            data:{
-                max_behot_time: curTime,
-                size: 20
-            },
-            success: function (data){
-                off = true;
-                time(data);
-                var html = template('news',data);
-                $('#newsList').append(html);
-            },
-            error: function (obj,status){
-                $('#nwsList').html('<li><h2>'+status+'</h2></li>');
-            }
-        })
-    }
-    function time(data){
-        for(var i = 0; i < data.detail.length; i++){
-            initTime(data.detail[i]['behot_time']);
-            if(i == data.detail.length-1){
-                curTime = data.detail[i]['behot_time'] - 1000;
-            }
-        }
-
-        function initTime(date){
-            var curdate = new Date(date);
-            var Years = curdate.getFullYear();
-            var Month = Double(curdate.getMonth()+1);
-            var Day = Double(curdate.getDate());
-            var Hours = Double(curdate.getHours());
-            var Minutes = Double(curdate.getMinutes());
-            var Seconds = Double(curdate.getSeconds());
-            data.detail[i]['time'] = Years+'年'+Month+'月'+Day+'日  '+Hours+':'+Minutes+':'+Seconds;
-            function Double(nub){
-                return nub = nub < 10?'0'+nub: ''+nub;
-            }
-            return date;
-        }
-    }
-
-};
+//window.onload = function (){
+//    var off = false;
+//    var curTime = new Date().getTime()*1000;
+//    ajax();
+//
+//    window.onscroll = function (){
+//        if(off){
+//            //off = false;
+//            console.log($('#newsList').height() ,window.pageYOffset)
+//            if($('#newsList').height() <= window.innerHeight + window.pageYOffset){
+//                ajax();
+//            }
+//        }
+//    };
+//    function ajax(){
+//        $.ajax({
+//            url:'http://api.1-blog.com/biz/bizserver/news/list.do',
+//            dataType:'json',
+//            data:{
+//                max_behot_time: curTime,
+//                size: 20
+//            },
+//            success: function (data){
+//                off = true;
+//                time(data);
+//                var html = template('news',data);
+//                $('#newsList').append(html);
+//            },
+//            error: function (obj,status){
+//                $('#nwsList').html('<li><h2>'+status+'</h2></li>');
+//            }
+//        })
+//    }
+//    function time(data){
+//        for(var i = 0; i < data.detail.length; i++){
+//            initTime(data.detail[i]['behot_time']);
+//            if(i == data.detail.length-1){
+//                curTime = data.detail[i]['behot_time'] - 1000;
+//            }
+//        }
+//
+//        function initTime(date){
+//            var curdate = new Date(date);
+//            var Years = curdate.getFullYear();
+//            var Month = Double(curdate.getMonth()+1);
+//            var Day = Double(curdate.getDate());
+//            var Hours = Double(curdate.getHours());
+//            var Minutes = Double(curdate.getMinutes());
+//            var Seconds = Double(curdate.getSeconds());
+//            data.detail[i]['time'] = Years+'年'+Month+'月'+Day+'日  '+Hours+':'+Minutes+':'+Seconds;
+//            function Double(nub){
+//                return nub = nub < 10?'0'+nub: ''+nub;
+//            }
+//            return date;
+//        }
+//    }
+//
+//};
 
